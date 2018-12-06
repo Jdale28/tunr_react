4af2bbd2f813a1a184b475cf6c14c3ec85636369
Jordan Dale <jordan.dale2@gmail.com>
2018-12-06 15:14:21 -0500

    Completed admin CRUD

--- tunr_react/urls.py
+++ tunr_react/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('main.urls')),
     url(r'^$', views.FrontendAppView.as_view())
 ]
